77d6fc6f2d0b45bb4e3bc4c4a7dea325ecfc99e2
Guilherme Félix Rosa <guilhermefelixrosa.dev@gmail.com>
2025-06-06 21:39:33 -0300

    Adicionando routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import express from "express"
-// import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client"
 // import cors from 'cors'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 const app = express()
 
+app.post("/usuarios", async (req, res) => {
+    await prisma.user.create({
+        data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+    });
+
+    res.status(201).json(req.body);
+})
 app.listen(3000); // Iniciando o servidor na porta 3000
 console.log("Servidor rodando: http://localhost:3000/"); // Mensagem de confirmação
